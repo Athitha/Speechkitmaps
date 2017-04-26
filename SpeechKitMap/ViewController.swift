2d1ee680dab7fb48c17b69eb5d3a042239b51506
athitha <athitha075@gmail.com>
2017-04-26 10:13:32 -0700

    first commit

---
+++ SpeechKitMap/ViewController.swift
@@ -0,0 +1,177 @@
+//
+//  ViewController.swift
+//  SpeechKitMap
+//
+//  
+//
+
+import UIKit
+import Speech
+import CoreLocation
+
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    
+    @IBOutlet weak var textview: UITextView!
+   
+    @IBOutlet weak var microphoneButton: UIButton!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))!
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        microphoneButton.isEnabled = false
+        
+        speechRecognizer.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
+    }
+    
+    @IBAction func Microphonetapped(_ sender: AnyObject) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+            if textview.text.lowercased().contains("siri") {
+                openMaps()
+            }
+            
+        } else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
+        }
+    }
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {  //1
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()  //2
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()  //3
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }  //4
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        } //5
+        
+        recognitionRequest.shouldReportPartialResults = true  //6
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in  //7
+            
+            var isFinal = false  //8
+            
+            if result != nil {
+                
+                self.textview.text = result?.bestTranscription.formattedString  //9
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {  //10
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.microphoneButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)  //11
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()  //12
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        textview.text = "Say something, I'm listening!"
+        
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            microphoneButton.isEnabled = true
+        } else {
+            microphoneButton.isEnabled = false
+        }
+    }
+    
+    func openMaps() {
+        if !textview.text.isEmpty && textview.text.lowercased().contains("siri") {
+            print(textview.text)
+            let geocoder = CLGeocoder()
+            let str = textview.text ?? "201 South 4th St 95112" 
+            geocoder.geocodeAddressString(str) { (placemarksOptional, error) -> Void in
+                if let placemarks = placemarksOptional {
+                    print("placemark| \(placemarks.first)")
+                    if let location = placemarks.first?.location {
+                        let query = "?ll=\(location.coordinate.latitude),\(location.coordinate.longitude)"
+                        let path = "http://maps.apple.com/" + query
+                        if let url = NSURL(string: path) {
+                            UIApplication.shared.openURL(url as URL)
+                        } else {
+                            print("Could not construct url. Handle error.")
+                        }
+                    } else {
+                        print("Could not get a location from the geocode request. Handle error.")
+                    }
+                } else {
+                    print("Didn't get any placemarks. Handle error.")
+                }
+            }
+        }
+        
+        
+    }
+}
+
